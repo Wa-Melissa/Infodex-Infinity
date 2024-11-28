7089cd7ae5ea18e7178256887291face4dc887d5
Florian S <contact@flo-x.fr>
2024-11-28 11:38:03 +0100

    added communication in gameover renderer

--- src/www/gameover/renderer.js
+++ src/www/gameover/renderer.js
@@ -5,7 +5,7 @@ const DOM = createDOMReferences({
 let success_photos = ["ali_ayadi-content.jpeg","cedric_wemmert-content.png"];
 let failure_photos = ["ali_ayadi-fache.png","cedric_wemmert-triste.png","matthieu_zimerman-fache.png"];
 
-let success = false;
+let success = document.location.href.endsWith("?success=true");
 
 if (success){
     let randomIndex = Math.floor(Math.random() * success_photos.length); //determine un index aléatoire 
