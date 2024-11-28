24834a7eecf404d755c27a20dd200f65cec1873b
Melissa Wanner <melissa.wanner@unistra.fr>
2024-11-28 11:37:11 +0100

    iky

--- src/www/gameover/renderer.js
+++ src/www/gameover/renderer.js
@@ -1,6 +1,14 @@
-const DOM = {
+const DOM = createDOMReferences({
+	photo: "#photo",
+});
 
-	
+let success_photos = ["ali_ayadi-content.jpeg","cedric_wemmert-content.png"];
+let failure_photos = ["ali_ayadi-fache.png","cedric_wemmert-triste.png","matthieu_zimerman-fache.png"];
+
+let success = false;
+
+if (success){
+    let randomIndex = Math.floor(Math.random() * success_photos.length); //determine un index aléatoire 
+	let chosenPicture = success_photos[randomIndex];
+   // DOM.photo.src = 
 }
-Object.freeze(DOM);
-
