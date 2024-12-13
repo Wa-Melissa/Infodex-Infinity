6c31035495b8fadcfc2e17cee7af49675798a46b
Florian S <contact@flo-x.fr>
2024-12-13 14:55:39 +0100

    commented gameover renderer.js

--- src/www/gameover/renderer.js
+++ src/www/gameover/renderer.js
@@ -3,17 +3,17 @@ const DOM = createDOMReferences({
     text: "#text",
 });
 
-let success_photos = ["ali_ayadi-content.jpeg","cedric_wemmert-content.jpg"];
-let failure_photos = ["ali_ayadi-fache.png","cedric_wemmert-triste.png","mathieu_zimmermann-fache.png"];
+const success_photos = ["ali_ayadi-content.jpeg","cedric_wemmert-content.jpg"];
+const failure_photos = ["ali_ayadi-fache.png","cedric_wemmert-triste.png","mathieu_zimmermann-fache.png"];
 //Actuellement chaque texte est associé à une photo spécifiquement
-let success_text = ["Tu as fais un travail excellent, c'est bien.","Tu as vu, c'était pas si difficile !"];
-let failure_text = ["Ça ne demandait pas la lune… et pourtant, tu t’es complètement planté !","Je t'ai surestimé.","Non mais c'est pas possible, tu as fait exploser la base ! C'est n'importe quoi."];
+const success_text = ["Tu as fais un travail excellent, c'est bien.","Tu as vu, c'était pas si difficile !"];
+const failure_text = ["Ça ne demandait pas la lune… et pourtant, tu t’es complètement planté !","Je t'ai surestimé.","Non mais c'est pas possible, tu as fait exploser la base ! C'est n'importe quoi."];
 
-let success = document.location.href.endsWith("?success=true");
+const success = document.location.href.endsWith("?success=true");
 
 //On choisit une photo aléatoirement
 if (success){
-    let randomIndex = Math.floor(Math.random() * success_photos.length);
+    const randomIndex = Math.floor(Math.random() * success_photos.length);
     DOM.photo.src = success_photos[randomIndex];
     DOM.text.innerHTML = success_text[randomIndex];
     DOM.text.innerHTML += "<br>Tu as atteint une satisfaction de " + Math.min(sessionSatisfaction.v,100) 
@@ -21,7 +21,7 @@ if (success){
     + "La base était corrompue à "+ ((sessionDbCorruptedCells.v / sessionDbTotalCells.v) * 100) + "%  ;) <br>";
 }
 else{
-    let randomIndex = Math.floor(Math.random() * failure_photos.length);
+    const randomIndex = Math.floor(Math.random() * failure_photos.length);
     DOM.photo.src = failure_photos[randomIndex];
     DOM.text.innerHTML = failure_text[randomIndex];
     DOM.text.innerHTML += "<br>Tu as été trop négligent.<br>";
